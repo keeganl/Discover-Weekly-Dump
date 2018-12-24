03bcd75b1950608f8a7c47cd032b0d4a4dbf1c27
Keegan Lawley <keeganlk.7@gmail.com>
2018-12-24 17:16:53 -0600

    Need to fix auth but getting songs

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 const SpotifyWebApi = require("spotify-web-api-node");
-var cron = require("node-cron");
 
 var spotifyApi = new SpotifyWebApi({
   clientId: "baf66a9db8d34ca48e2e860c120de0a4",
-  clientSecret: "52f993759d694b2aac17e663dea4e9c0",
-  redirectUri: "http://www.example.com/callback"
+  clientSecret: "8426e3f40dc441369615fbb7aac1e0e3",
 });
 
+var tracks = [];
+
+// Passing a callback - get Elvis' albums in range [20...29]
 spotifyApi.clientCredentialsGrant()
   .then(function (data) {
     console.log('The access token expires in ' + data.body['expires_in']);
     console.log('The access token is ' + data.body['access_token']);
-
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token']);
-    var tracks = []
     spotifyApi
       .getPlaylist("37i9dQZEVXcEFGwy4vib4y")
-      .then(
-        function(data) {
-          for (const i of data.body.tracks.items) {
-            console.log(`${i.track.name}`);
-            console.log(`${i.track.uri}`);
-            console.log(`${i.track.href}\n`);
-            tracks.push(i.track.uri);
-          }
-          return tracks;
-        }, function(err) {
+      .then(function (data) {
+        // console.log("Retrieved playlists", data.body);
+        // console.log(data.body.items[0].tracks);
+        for (const i of data.body.tracks.items) {
+          console.log(`${i.track.name}`);
+          console.log(`${i.track.uri}`);
+          console.log(`${i.track.href}\n`);
+          tracks.push(i.track.uri);
+        }
+        return tracks;
+      }, function (err) {
         console.log("Something went wrong!", err);
+      }, function (err) {
+        console.log(err);
       })
-      .then(
-        // 4pTSMdTFjdf4R4K71WHTwI
-        function(tracks) {
-          console.log(tracks)
+      .then(// 4pTSMdTFjdf4R4K71WHTwI
+        function (tracks) {
           return tracks.forEach(track => {
-            spotifyApi.addTracksToPlaylist('4pTSMdTFjdf4R4K71WHTwI', tracks)
-              .then(function (data) {
-                console.log(`Added ${data} to playlist!`);
-              }, function (err) {
-                console.log('Something went wrong!', err);
-              });
+            // console.log(track);
+            spotifyApi.addTracksToPlaylist("4pTSMdTFjdf4R4K71WHTwI", track, function (err) {
+              console.log(err);
+            }).then(() => {
+              console.log("success")
+            }, function (err) {
+              console.log(err);
+            });
           });
-      }) 
-  });
+        })  
+})
+
