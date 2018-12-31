407b3a9a60c5aa47848ea675758d5f45b3b2f36a
Keegan Lawley <keeganlk.7@gmail.com>
2018-12-31 14:16:19 -0600

    Works as intended

--- index.js
+++ index.js
@@ -1,48 +1,83 @@
 const SpotifyWebApi = require("spotify-web-api-node");
+const readline = require("readline");
+
+var tracks = [];
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var scopes = [
+  "playlist-read-private",
+  "playlist-modify-private",
+  "playlist-modify-public"
+];
+
+var state = "";
 
 var spotifyApi = new SpotifyWebApi({
   clientId: "baf66a9db8d34ca48e2e860c120de0a4",
   clientSecret: "8426e3f40dc441369615fbb7aac1e0e3",
+  redirectUri: "https://example.com/callback"
 });
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+console.log(authorizeURL);
 
-var tracks = [];
+console.log("Open that url in a browser and paste the result below:");
 
-// Passing a callback - get Elvis' albums in range [20...29]
-spotifyApi.clientCredentialsGrant()
-  .then(function (data) {
-    console.log('The access token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi
-      .getPlaylist("37i9dQZEVXcEFGwy4vib4y")
-      .then(function (data) {
-        // console.log("Retrieved playlists", data.body);
-        // console.log(data.body.items[0].tracks);
-        for (const i of data.body.tracks.items) {
-          console.log(`${i.track.name}`);
-          console.log(`${i.track.uri}`);
-          console.log(`${i.track.href}\n`);
-          tracks.push(i.track.uri);
-        }
-        return tracks;
-      }, function (err) {
-        console.log("Something went wrong!", err);
-      }, function (err) {
-        console.log(err);
-      })
-      .then(// 4pTSMdTFjdf4R4K71WHTwI
+rl.question("", loc => {
+  var a = loc.split("code=")
+  var b = a[1].split('&state=')
+  let code = b[0];
+  console.log(`${code}`);
+
+  // Retrieve an access token and a refresh token
+  spotifyApi.authorizationCodeGrant(code).then(
+    function (data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']);
+
+      // Set the access token on the API object to use it in later calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+      spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+      spotifyApi
+        .getPlaylist("37i9dQZEVXcEFGwy4vib4y")
+        .then(function (data) {
+          for (const i of data.body.tracks.items) {
+            console.log(`${i.track.name}`);
+            console.log(`${i.track.uri}`);
+            console.log(`${i.track.href}\n`);
+            tracks.push(i.track.uri);
+          }
+          return tracks;
+          }, function (err) {
+            console.log("Something went wrong!", err);
+          }, function (err) {
+            console.log(err);
+          }
+      ).then(// 4pTSMdTFjdf4R4K71WHTwI
         function (tracks) {
-          return tracks.forEach(track => {
-            // console.log(track);
-            spotifyApi.addTracksToPlaylist("4pTSMdTFjdf4R4K71WHTwI", track, function (err) {
-              console.log(err);
-            }).then(() => {
-              console.log("success")
-            }, function (err) {
-              console.log(err);
-            });
-          });
+          spotifyApi
+            .addTracksToPlaylist(
+              "4pTSMdTFjdf4R4K71WHTwI",
+              tracks,
+              function (err) {
+                console.log(err);
+              }
+            )
         })  
-})
+    },
+    function (err) {
+      console.log('Something went wrong!', err);
+    }
+  );
+
+  //open.spotify.com/playlist/4pTSMdTFjdf4R4K71WHTwI?si=EKALJTRrQ8-DAFnpJp4fKw
+
+  rl.close();
+});
 
